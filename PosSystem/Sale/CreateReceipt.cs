d1b6e5064042ca6afd8747a3044c93f4b12cc2cb
tartie sorel <sorelaugustin@gmail.com>
2020-10-22 21:42:31 +0100

    Print receipt with full details

---
+++ PosSystem/Sale/CreateReceipt.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace PosSystem
+{
+    internal class CreateReceipt
+    {
+        private readonly ListView listView1;
+        private readonly string workerID;
+        private readonly string date;
+        private readonly Font font = new Font("Courier New", 12);
+        private Graphics graphic;
+
+        private const int startX = 10;
+        private const int startY = 10;
+        private int offset = 40;
+        private float cash = 50; // Change this
+        private float totalprice = 0.00f;
+        private float change = 0;
+        private float fontHeight = 0;
+
+        public CreateReceipt(ListView listView1, string workerID, string date)
+        {
+            this.listView1 = listView1;
+            this.workerID = workerID;
+            this.date = date;
+
+            PrintDialog printDialog = new PrintDialog();
+            PrintDocument printDocument = new PrintDocument();
+
+            printDialog.Document = printDocument;
+            printDocument.PrintPage += new PrintPageEventHandler(GenerateReceipt);
+
+            DialogResult result = printDialog.ShowDialog();
+            if (result == DialogResult.OK)
+                printDocument.Print();
+        }
+
+        public void GenerateReceipt(object sender, PrintPageEventArgs e)
+        {
+            graphic = e.Graphics;
+            fontHeight = font.GetHeight();
+
+            DrawTop();
+            DrawItems();
+            DrawBottom();
+        }
+
+        private void DrawBottom()
+        {
+            graphic.DrawString("Total to pay ".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 12, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
+
+            offset += 30;
+            graphic.DrawString("CASH ".PadRight(30) + String.Format("{0:c}", cash), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += 15;
+            graphic.DrawString("CHANGE ".PadRight(30) + String.Format("{0:c}", change), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += 30;
+
+            graphic.DrawString("WORKER ID ".PadRight(30) + String.Format("{0:c}", workerID), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += 15;
+            graphic.DrawString("DATE ".PadRight(30) + String.Format("{0:c}", date), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += 30;
+
+
+            graphic.DrawString("     Thank-you for your custom,", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += 15;
+            graphic.DrawString("       please come back soon!", font, new SolidBrush(Color.Black), startX, startY + offset);
+        }
+
+        private void DrawItems()
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                string productBarCode = listView1.Items[i].SubItems[0].Text;
+                string quantity = listView1.Items[i].SubItems[1].Text;
+                float productPrice = float.Parse(listView1.Items[i].SubItems[3].Text);
+
+                totalprice += productPrice;
+
+                string productLine = productBarCode;
+
+                graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, startY + offset);
+                graphic.DrawString(quantity, font, new SolidBrush(Color.Black), startX + 200, startY + offset);
+                graphic.DrawString(productPrice.ToString(), font, new SolidBrush(Color.Red), startX + 325, startY + offset);
+
+                offset = offset + (int)fontHeight + 5;
+            }
+
+            change = cash - totalprice;
+            offset += 20;
+        }
+
+        private void DrawTop()
+        {
+            graphic.DrawString(" Toy Shop Test", new Font("Courier New", 18), new SolidBrush(Color.Black), startX, startY);
+            string top = "Item CodeBar".PadRight(15) + "Quantity".PadRight(15) + "Price";
+            graphic.DrawString(top, font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset += (int)fontHeight;
+            graphic.DrawString("-----------------------------------", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight + 5;
+        }
+    }
+}
--- PosSystem/Sale/Sale.Designer.cs
+++ PosSystem/Sale/Sale.Designer.cs
@@ -52,7 +52,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
+            this.checkBoxPrintReceipt = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxItem)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -259,7 +259,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.checkBoxPrintReceipt);
             this.groupBox2.Controls.Add(this.listView1);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
@@ -312,15 +312,16 @@
             this.panel2.Size = new System.Drawing.Size(550, 54);
             this.panel2.TabIndex = 13;
             // 
-            // button3
+            // checkBoxPrintReceipt
             // 
-            this.button3.Location = new System.Drawing.Point(543, 187);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            this.checkBoxPrintReceipt.AutoSize = true;
+            this.checkBoxPrintReceipt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.checkBoxPrintReceipt.Location = new System.Drawing.Point(487, 238);
+            this.checkBoxPrintReceipt.Name = "checkBoxPrintReceipt";
+            this.checkBoxPrintReceipt.Size = new System.Drawing.Size(140, 29);
+            this.checkBoxPrintReceipt.TabIndex = 12;
+            this.checkBoxPrintReceipt.Text = "Print Receipt";
+            this.checkBoxPrintReceipt.UseVisualStyleBackColor = true;
             // 
             // Sale
             // 
@@ -373,6 +374,6 @@
         public System.Windows.Forms.Label lblDisplayFinalPrice;
         public System.Windows.Forms.Label lblDescriptionDisplay;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBoxPrintReceipt;
     }
 }
