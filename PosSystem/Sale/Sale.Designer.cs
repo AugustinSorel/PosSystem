027ead1dea860e15ad1bb6b76d7d3a2c7d8b5bc1
tartie sorel <sorelaugustin@gmail.com>
2020-10-22 21:06:45 +0100

    Receipt

--- PosSystem/Sale/Sale.Designer.cs
+++ PosSystem/Sale/Sale.Designer.cs
@@ -39,6 +39,7 @@
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDescriptionDisplay = new System.Windows.Forms.Label();
             this.lblItemIDDisplay = new System.Windows.Forms.Label();
             this.lblItemID = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -50,8 +51,8 @@
             this.lblDisplayFinalPrice = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblDescriptionDisplay = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxItem)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -172,6 +173,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Item Description";
             // 
+            // lblDescriptionDisplay
+            // 
+            this.lblDescriptionDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblDescriptionDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.lblDescriptionDisplay.Location = new System.Drawing.Point(495, 19);
+            this.lblDescriptionDisplay.Name = "lblDescriptionDisplay";
+            this.lblDescriptionDisplay.Size = new System.Drawing.Size(132, 62);
+            this.lblDescriptionDisplay.TabIndex = 32;
+            // 
             // lblItemIDDisplay
             // 
             this.lblItemIDDisplay.AutoSize = true;
@@ -249,6 +259,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.listView1);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
@@ -292,15 +303,6 @@
             this.panel1.Size = new System.Drawing.Size(194, 52);
             this.panel1.TabIndex = 12;
             // 
-            // lblDescriptionDisplay
-            // 
-            this.lblDescriptionDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.lblDescriptionDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
-            this.lblDescriptionDisplay.Location = new System.Drawing.Point(495, 19);
-            this.lblDescriptionDisplay.Name = "lblDescriptionDisplay";
-            this.lblDescriptionDisplay.Size = new System.Drawing.Size(132, 62);
-            this.lblDescriptionDisplay.TabIndex = 32;
-            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.LblWorkerID);
@@ -309,6 +311,16 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(550, 54);
             this.panel2.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(543, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Sale
             // 
@@ -361,5 +373,6 @@
         public System.Windows.Forms.Label lblDisplayFinalPrice;
         public System.Windows.Forms.Label lblDescriptionDisplay;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- PosSystem/Sale/Sale.cs
+++ PosSystem/Sale/Sale.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
 using System.Windows.Forms;
 
 namespace PosSystem
@@ -96,6 +98,78 @@ namespace PosSystem
         {
             if (textBox1.Text.Length == 13 && e.KeyCode != Keys.Back)
                 button1.PerformClick();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            PrintDocument printDocument = new PrintDocument();
+
+            printDialog.Document = printDocument;         
+            printDocument.PrintPage += new PrintPageEventHandler(CreateReceipt); 
+
+            DialogResult result = printDialog.ShowDialog();
+            if (result == DialogResult.OK)
+                printDocument.Print();
+        }
+
+        public void CreateReceipt(object sender, PrintPageEventArgs e)
+        {
+            float cash = 50; // Change this
+            float change = 0.00f;
+
+            Graphics graphic = e.Graphics;
+
+            Font font = new Font("Courier New", 12); 
+
+            float fontHeight = font.GetHeight();
+
+            int startX = 10;
+            int startY = 10;
+            int offset = 40;
+
+            graphic.DrawString(" Toy Shop Shop Test", new Font("Courier New", 18), new SolidBrush(Color.Black), startX, startY);
+            string top = "Item CodeBar".PadRight(15) + "Quantity".PadRight(15) + "Price";
+            graphic.DrawString(top, font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight; 
+            graphic.DrawString("-----------------------------------", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight + 5; 
+
+            float totalprice = 0.00f;
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                string productBarCode = listView1.Items[i].SubItems[0].Text;
+                string quantity = listView1.Items[i].SubItems[1].Text;
+                float productPrice = float.Parse(listView1.Items[i].SubItems[3].Text);
+
+                totalprice += productPrice;
+
+                string productLine = productBarCode;
+
+                graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, startY + offset);
+                graphic.DrawString(quantity, font, new SolidBrush(Color.Black), startX + 200, startY + offset);
+                graphic.DrawString(productPrice.ToString(), font, new SolidBrush(Color.Red), startX + 325, startY + offset);
+
+                offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+            }
+
+            change = (cash - totalprice);
+
+            offset = offset + 20; 
+
+            graphic.DrawString("Total to pay ".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 12, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
+
+            offset = offset + 30; 
+            graphic.DrawString("CASH ".PadRight(30) + String.Format("{0:c}", cash), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 15;
+            graphic.DrawString("CHANGE ".PadRight(30) + String.Format("{0:c}", change), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 30; 
+            graphic.DrawString("     Thank-you for your custom,", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 15;
+            graphic.DrawString("       please come back soon!", font, new SolidBrush(Color.Black), startX, startY + offset);
+
         }
     }
 }
